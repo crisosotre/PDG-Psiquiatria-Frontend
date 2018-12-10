a78e777aea2bda9064df61d69595052316ba6e6f
crisosotre <crisosotre@hotmail.com>
2018-12-09 22:24:18 -0500

    login

--- src/app/components/resultados-estudiante/resultados-estudiante.component.ts
+++ src/app/components/resultados-estudiante/resultados-estudiante.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResultadosEstudianteComponent implements OnInit {
 
+  //Grafica de lineas
+  public lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType:string = 'line';
+
   constructor() { }
 
   ngOnInit() {
--- src/app/components/resultados-profesor/resultados-profesor.component.ts
+++ src/app/components/resultados-profesor/resultados-profesor.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { BrowserTransferStateModule } from '@angular/platform-browser';
-import { routerNgProbeToken } from '@angular/router/src/router_module';
+
 
 @Component({
   selector: 'app-resultados-profesor',
@@ -11,6 +10,7 @@ export class ResultadosProfesorComponent implements OnInit {
 
   curso: string;
 
+  //Grafica de dona
   public doughnutChartLabels:string[] = ['Saber: 33.3%', 'Hacer: 33.3%', 'Ser: 33.3%'];
   public doughnutChartData:number[] = [33.3, 33.3, 33.3];
   public doughnutChartType:string = 'doughnut';
@@ -42,6 +42,31 @@ export class ResultadosProfesorComponent implements OnInit {
   }
   };
 
+  //Grafica de lineas
+  public lineChartData:Array<any> = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  public lineChartLabels:Array<any> = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  public lineChartType:string = 'line';
+
+//Grafico de barras
+  public barChartOptions:any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
+  public barChartLabels:string[] = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  public barChartType:string = 'bar';
+  public barChartLegend:boolean = true;
+  public barChartData:any[] = [
+    {data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A'},
+    {data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B'}
+  ];
+
+//Grafica de torta
+  public pieChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail Sales'];
+  public pieChartData:number[] = [300, 500, 100];
+  public pieChartType:string = 'pie';
   constructor() { }
 
   ngOnInit() {
